3b62aa03969fc1bf6366e4e5ccdacbecd250ccd9
Jianwen WEI <weijianwen@gmail.com>
2017-10-28 17:18:03 +0800

    Replace service with systemctl. Add a variable to set SLURM service name.

--- manifests/service.pp
+++ manifests/service.pp
@@ -6,7 +6,7 @@ class slurm::service (
   $ensure       = $slurm::service_ensure,
   $enable       = $slurm::service_enable,
   $service      = $slurm::service_name,
-  $service_cmd  = 'slurm',
+  $service_cmd  = $slurm::service_cmd,
 ) {
 
   service { $service:
@@ -14,9 +14,9 @@ class slurm::service (
     enable      => $enable,
     hasstatus   => true,
     hasrestart  => true,
-    status      => "/usr/bin/service ${service_cmd} status",
-    start       => "/usr/bin/service ${service_cmd} start",
-    stop        => "/usr/bin/service ${service_cmd} stop",
-    restart     => "/usr/bin/service ${service_cmd} restart",  }
+    status      => "/usr/bin/systemctl status  ${service_cmd}",
+    start       => "/usr/bin/systemctl start   ${service_cmd}",
+    stop        => "/usr/bin/systemctl stop    ${service_cmd}",
+    restart     => "/usr/bin/systemctl restart ${service_cmd}",  }
 
 }
