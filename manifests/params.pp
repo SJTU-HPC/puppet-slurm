23bf1fd760fddf82617edd54ec6f60c0a598ed31
Jianwen WEI <weijianwen@gmail.com>
2019-06-13 16:37:29 +0800

    Fix select issues.

--- manifests/params.pp
+++ manifests/params.pp
@@ -3,10 +3,12 @@ class slurm::params {
 
   case $::osfamily {
     'RedHat': {
-      $package_name          = ['slurm', 'slurm-contribs', 'slurm-pam_slurm', 'slurm-perlapi', 'slurm-slurmctld', 'slurm-slurmd', 'slurm-libpmi', 'slurm-devel', 'slurm-contribs', 'slurm-example-configs']
+      $package_name = ['slurm', 'slurm-contribs', 'slurm-pam_slurm', 'slurm-perlapi', 'slurm-slurmctld', 'slurm-slurmd', 'slurm-libpmi', 'slurm-devel', 'slurm-contribs', 'slurm-example-configs']
+    }
     'Debian': {
-      $package_name          = ['slurmctld', 'slurmd', 'slurm-client', 'slurm-wlm-basic-plugins', 'libpam-slurm', 'libpmi0', 'libpmi2-0']
-          default: {
+      $package_name = ['slurmctld', 'slurmd', 'slurm-client', 'slurm-wlm-basic-plugins', 'libpam-slurm', 'libpmi0', 'libpmi2-0']
+    }
+    default: {
       fail("The ${module_name} module is not supported on an ${::osfamily} based system.")
     }
   }
